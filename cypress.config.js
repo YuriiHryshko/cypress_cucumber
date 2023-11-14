103838a7bb12b93db0cb9a393eb00e47945ef234
yuriygrishko <useru55389@gmail.com>
2023-11-14 15:51:04 +0200

    report

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,11 @@ const { defineConfig } = require("cypress");
 const cucumber = require('cypress-cucumber-preprocessor').default;
 
 module.exports = defineConfig({
+    reporter: 'cypress-mochawesome-reporter',
     e2e: {
         setupNodeEvents(on, config) {
-            on('file:preprocessor', cucumber())
+            on('file:preprocessor', cucumber());
+            require('cypress-mochawesome-reporter/plugin')(on);
         },
         specPattern: "cypress/e2e/features/*.feature",
         baseUrl: "https://www.saucedemo.com/",
